bf3e0a8b27d74c9af953d69a0ebe9be78dfc8f74
yoko2409 <21010420@anabuki-college.ac.jp>
2023-11-16 11:39:36 +0900

    ADD

--- aulaproject/settings.py
+++ aulaproject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'img')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
