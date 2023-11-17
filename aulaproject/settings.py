d0e29a39aaa6216d87de5c0575ada0b2e9d37881
yoko2409 <21010420@anabuki-college.ac.jp>
2023-11-17 10:23:58 +0900

    ADD

--- aulaproject/settings.py
+++ aulaproject/settings.py
@@ -76,13 +76,10 @@ WSGI_APPLICATION = 'aulaproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
