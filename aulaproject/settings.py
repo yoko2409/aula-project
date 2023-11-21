4eb0d1b88318038fb27936eabc1a0f1a14aa9c84
yoko2409 <21010420@anabuki-college.ac.jp>
2023-11-21 14:04:51 +0900

    ADD

--- aulaproject/settings.py
+++ aulaproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-52f!wune3@^-m++f3enbn$zicqteblo!zct&5og9mrnuz0xiu#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'aula-project-cb9dc2cc4ed6.herokuapp.com/']
 
